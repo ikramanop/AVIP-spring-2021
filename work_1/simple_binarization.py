fab36600c4c5b14512f8f2bce68b7c7a732c79e6
Лев Мардер <l.marder@MacBook-Pro-Lev.local>
2021-02-13 23:25:58 +0300

    Initial commit

---
+++ work_1/simple_binarization.py
@@ -0,0 +1,22 @@
+from PIL import Image, ImageDraw
+from semitone import semitone
+import numpy as np
+
+
+def simple_binarization(old_image, threshold):
+    semi = semitone(old_image)
+    new_image = np.zeros(shape=semi.shape)
+
+    new_image[semi > threshold] = 255
+
+    return new_image.astype(np.uint8)
+
+
+if __name__ == '__main__':
+    image_name = 'random_text.jpg'
+    method_prefix = 'semitone'
+    img_src = Image.open('pictures_src/' + image_name).convert('RGB')
+    img_src_arr = np.array(img_src)
+
+    Image.fromarray(simple_binarization(
+        img_src_arr, 128), 'L').show()
